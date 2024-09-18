2a0f30fcdcc4433730212b8266561b0d371d67d5
uuip <uuip@users.noreply.github.com>
2024-09-18 16:20:16 +0800

    update

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use eyre::{anyhow, Result};
+use tokio::task;
+
+#[tokio::main]
+async fn main() {
+    let tasks: Vec<_> = [1, 2, 3, 4, 5]
+        .into_iter()
+        .map(|i| task::spawn(async move { doit(i).await }))
+        .collect();
+    let results = futures::future::join_all(tasks).await;
+    for x in results.into_iter().flatten().flatten() {
+        println!("{}", x);
+    }
+}
+
+async fn doit(i: i32) -> Result<i32> {
+    if i % 2 == 0 {
+        Err(anyhow!("something"))
+    } else {
+        Ok(i)
+    }
+}
