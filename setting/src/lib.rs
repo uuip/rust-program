a709cc89b1fa676dcce52eee8a9d60b78a4ea631
uuip <uuip@users.noreply.github.com>
2025-01-29 15:41:47 +0800

    update

--- setting/src/lib.rs
+++ setting/src/lib.rs
@@ -1,32 +1,42 @@
-/// 对于Option字段，使用值时：setting.explorer_db.as_ref().unwrap()
-#[derive(Debug)]
+use dotenvy::dotenv;
+use serde::Deserialize;
+
+/// usage:
+/// ```
+/// use setting::Setting;
+/// use std::sync::LazyLock;
+///
+/// static SETTING: LazyLock<Setting, fn() -> Setting> = LazyLock::new(Setting::init);
+/// ```
+#[derive(Debug, Deserialize)]
 pub struct Setting {
-    pub pulsar_addr: Option<String>,
-    pub topic: Option<String>,
     pub explorer_db: Option<String>,
+    #[serde(rename = "db_url")]
     pub db: String,
     pub rpc_list: Option<Vec<String>>,
 }
 
-pub fn get_env(key: &str) -> Option<String> {
-    dotenvy::var(key).ok()
-}
-
 impl Setting {
     pub fn init() -> Self {
-        let pulsar_addr = get_env("PULSAR_URL");
-        let topic = get_env("PULSAR_TOPIC");
-        let explorer_db = get_env("EXPLORER_DB");
-        let db = get_env("DB_URL").expect("DB_URL not set");
-        let rpc_list =
-            get_env("rpc_list").map(|s| s.split(',').map(str::to_string).collect::<Vec<String>>());
-
-        Self {
-            pulsar_addr,
-            topic,
-            explorer_db,
-            db,
-            rpc_list,
-        }
+        dotenv().ok();
+        envy::from_env::<Setting>().unwrap()
     }
 }
+
+// use config::{Config, Environment};
+//
+// impl Setting {
+//     pub fn init() -> Self {
+//         dotenv().ok();
+//
+//         Config::builder()
+//             .add_source(
+//                 Environment::default()
+//                     .try_parsing(true)
+//                     .list_separator(",")
+//                     .with_list_parse_key("rpc_list"),
+//             )
+//             .build().and_then(|c|c.try_deserialize::<Setting>())
+//             .unwrap()
+//     }
+// }
